8bffe69ba4c7f5f04407c6937973fe99a28d08b5
SURAJ MAURYA <121401103+suraj2510@users.noreply.github.com>
2023-11-09 22:46:58 +0530

    Add files via upload

---
+++ sideBar Menu/script.js
@@ -0,0 +1,7 @@
+var sideLine=document.querySelector(".ri-align-justify");
+
+var menu = document.querySelector(".menu")
+
+sideLine.addEventListener("click",(function(){
+    menu.classList.toggle("show-menu")
+}))
